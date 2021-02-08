2c7dfd62a8f058f8dc962d4ba88abcb6b504ceca
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 15:19:48 -0300

    carp estructura if

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace estructura_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //estructura if
+
+            byte juan = 20, pedro = 19, jorge = 25;
+
+            if (juan > pedro)
+            {
+                if (jorge > juan)
+                {
+                    Console.WriteLine("jorge es el mayor de todos");
+
+                }
+            }
+
+            Console.ReadKey();
+
+           
+
+
+
+
+        }
+    }
+}
